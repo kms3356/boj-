a44d537fc4fb0f767592ac0d0c7edb2138b30184
kms3356 <john563322@gmail.com>
2026-04-01 15:30:07 +0900

    [Silver V] Title: 슈퍼 소수, Time: 48 ms, Memory: 37580 KB -BaekjoonHub

--- 백준/Silver/31216. 슈퍼 소수/슈퍼 소수.py
+++ 백준/Silver/31216. 슈퍼 소수/슈퍼 소수.py
@@ -1,5 +1,4 @@
 import sys
-input = sys.stdin.readline
 def sieve_of_eratosthenes():
     sieve = [True] * (318138)
     sieve[0] = sieve[1] = False
@@ -8,17 +7,17 @@ def sieve_of_eratosthenes():
         if sieve[i]:
             sieve[i*i : 318138 : i] = [False] * len(range(i*i, 318138, i))
 
-    primes = [i for i in range(2, 318138) if sieve[i]]
-    sup = []
-    for idx, prime in enumerate(primes):
-        if sieve[idx+1]:
-            sup.append(prime)
+    primes = [2] + [i for i in range(3, 318138, 2) if sieve[i]]
+    sup = [primes[i] for i in range(len(primes)) if sieve[i+1]]
     return sup
 
 def solve():
-    t = int(input())
+    inp = map(int, sys.stdin.read().split())
+    t = next(inp)
+    res = []
     sup = sieve_of_eratosthenes()
     for _ in range(t):
-        n = int(input())
-        print(sup[n-1])
+        n = next(inp)
+        res.append(str(sup[n-1]))
+    sys.stdout.write('\n'.join(res))
 solve()

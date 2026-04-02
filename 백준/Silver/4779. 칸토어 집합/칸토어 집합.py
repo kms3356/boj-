2bd06c64e0ad703d2d075dbd47cb114276055e70
kms3356 <john563322@gmail.com>
2026-04-02 15:11:35 +0900

    [Silver III] Title: 칸토어 집합, Time: 32 ms, Memory: 33796 KB -BaekjoonHub

--- 백준/Silver/4779. 칸토어 집합/칸토어 집합.py
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.py
@@ -1,14 +1,14 @@
 import sys
 
 def canto(st):
-    if len(st) == 1: return st
-    size = len(st)//3
-    side = canto(st[:size])
+    if st == 1: return '-'
+    size = st//3
+    side = canto(size)
     center = " " * size
     return side + center + side
 
 inp = map(int, sys.stdin.read().split())
 
 for n in inp:
-    st = "-" * (3**n)
+    st = 3**n
     print(canto(st))

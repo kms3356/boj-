bc9f143c745f1f8cbf9cb5484e6d8b8bf2cb3999
kms3356 <john563322@gmail.com>
2026-04-02 14:02:11 +0900

    [Silver IV] Title: 동전 0, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -0,0 +1,17 @@
+import sys
+
+def solve():
+    inp = map(int, sys.stdin.read().split())
+    n = next(inp)
+    k = next(inp)
+    A = list(inp)
+    count = 0
+    for i in range(n-1, -1, -1):
+        a, b = divmod(k,A[i])
+        if a:
+            count += a
+            if b == 0:
+                break
+            k = b
+    print(count)
+solve()

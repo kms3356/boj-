153dc71d39768702b1158ed779b2f98001ef93b4
kms3356 <john563322@gmail.com>
2026-04-02 13:17:36 +0900

    [Silver IV] Title: 설탕 배달, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -0,0 +1,20 @@
+import sys
+def solve():
+    n = int(sys.stdin.readline())
+    ans = 0
+    while True:
+        if n%5==0:
+            ans += n//5
+            break
+        
+        if 0 < n < 3:
+            ans = -1
+            break
+        elif n == 0:
+            break
+        n -= 3
+        ans += 1
+    print(ans)
+        
+
+solve()

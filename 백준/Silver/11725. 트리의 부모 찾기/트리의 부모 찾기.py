5656a9a6e04314e539373ba72124619493dfb97f
kms3356 <john563322@gmail.com>
2026-04-06 15:20:10 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 260 ms, Memory: 53176 KB -BaekjoonHub

---
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+from collections import deque
+def solve():
+    n = int(input())
+    res = [[] for _ in range(n+1)]
+    for _ in range(n-1):
+        n1, n2 = map(int, input().split())
+        res[n1].append(n2)
+        res[n2].append(n1)
+    
+    visited = [0] * (n+1)
+    queue = deque([1])
+    visited[1] = 1
+    while(queue):
+        cur = queue.popleft()
+
+        for post in res[cur]:
+            if not visited[post]:
+                queue.append(post)
+                visited[post] = cur
+    print(*visited[2:], sep='\n')
+solve()

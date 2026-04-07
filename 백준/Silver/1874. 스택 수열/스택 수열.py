0cd6f31b2ddad155eb5d0204736d4d7577d248f9
kms3356 <john563322@gmail.com>
2026-04-07 19:56:28 +0900

    [Silver II] Title: 스택 수열, Time: 72 ms, Memory: 40392 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.py
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -1,32 +1,20 @@
 import sys
-
 def solve():
-    input_data = sys.stdin.read().split()
-    
-    n = int(input_data[0])
-    target_sequence = list(map(int, input_data[1:]))
-    
+    inp = map(int, sys.stdin.read().split())
+    n = next(inp)
     stack = []
-    result = []
-    current_num = 1  
-    possible = True
-    
-    for target in target_sequence:
-        while current_num <= target:
-            stack.append(current_num)
-            result.append("+")
-            current_num += 1
-        
-        if stack[-1] == target:
+    res = []
+    i = 1
+    for cur in inp:
+        while i <= cur:
+            stack.append(i)
+            res.append('+')
+            i += 1
+        if stack[-1] == cur:
             stack.pop()
-            result.append("-")
+            res.append('-')
         else:
-            possible = False
-            break
-
-    if possible:
-        print("\n".join(result))
-    else:
-        print("NO")
-
+            print("NO")
+            return
+    sys.stdout.write('\n'.join(res))
 solve()

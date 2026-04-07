d07244522f765c89c5ed51b1f59e814b077070c3
kms3356 <john563322@gmail.com>
2026-04-07 19:46:48 +0900

    [Silver III] Title: 1로 만들기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,18 +1,8 @@
-import sys
-def solve():
-    x = int(sys.stdin.readline())
-    res = [0,0,1,1,2,3,2,3,3,2,3,4,3,4,4,4,4] + [0] * (x-15)
-    if x <= 16:
-        print(res[x])
-    else:
-        for i in range(17, x+1):
-            if i%2==0 and i%3==0:
-                res[i] = min(res[i-1], res[i//2], res[i//3]) + 1
-            elif i%2==0:
-                res[i] = min(res[i-1], res[i//2]) + 1
-            elif i%3==0:
-                res[i] = min(res[i-1], res[i//3]) + 1
-            else:
-                res[i] = res[i-1] + 1
-        print(res[x])
-solve()
+def f(n):
+    if n in dic.keys() : return dic[n]
+    cur_cal = min(f(n//2)+n%2, f(n//3)+n%3) + 1
+    dic[n] = cur_cal
+    return cur_cal
+
+dic = {1:0, 2:1, 3:1}
+print(f(int(input())))

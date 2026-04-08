0a0d8a1017a9bb303f1580091e5a1ec52ad5fbe5
kms3356 <john563322@gmail.com>
2026-04-08 10:02:04 +0900

    [Silver IV] Title: 설탕 배달, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,15 +1,18 @@
+import sys
 def solve():
-    n = int(input())
-    min = n//5 + n//3
-    isChange = False
-    for a5 in range(n//5 + 1):
-        for a3 in range(n//3 + 1):
-            if a5*5 + a3*3 == n:
-                if min >= a5+a3: 
-                    isChange = True
-                    min = a5+a3
-    if isChange:
-        print(min)
-    else:
-        print(-1)
+    n = int(sys.stdin.readline())
+    ans = 0
+    while True:
+        if n%5==0:
+            ans += n//5
+            break
+        
+        if 0 < n < 3:
+            ans = -1
+            break
+        elif n == 0:
+            break
+        n -= 3
+        ans += 1
+    print(ans)
 solve()

5b28d356c915fc281ccd33c9f5a6383a00d39647
kms3356 <john563322@gmail.com>
2026-04-08 13:50:40 +0900

    [Silver V] Title: 배열 합치기, Time: 1060 ms, Memory: 312668 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.py
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.py
@@ -1,18 +1,28 @@
 import sys
 input = sys.stdin.readline
+def solve():
+    n,m = map(int,input().split())
+    A = list(map(int, input().split()))
+    B = list(map(int, input().split()))
+    if A[0] >= B[-1]:
+        B.extend(A)
+        sys.stdout.write(' '.join(map(str,B)))
+        return
+    if B[0] >= A[-1]:
+        A.extend(B)
+        sys.stdout.write(' '.join(map(str,A)))
+        return
+    
+    res = []
+    i=j=0
+    while i < n and j < m:
+        if A[i] <= B[j]:
+            res.append(A[i])
+            i+=1
+        else:
+            res.append(B[j])
+            j+=1
 
-n,m = map(int,input().split())
-A = list(map(int, input().split()))
-B = list(map(int, input().split()))
-res = []
-i=j=0
-while i < n and j < m:
-    if A[i] <= B[j]:
-        res.append(A[i])
-        i+=1
-    else:
-        res.append(B[j])
-        j+=1
-
-res.extend(A[i:] if i < n else B[j:])
-sys.stdout.write(' '.join(map(str,res)))
+    res.extend(A[i:] if i < n else B[j:])
+    sys.stdout.write(' '.join(map(str,res)))
+solve()

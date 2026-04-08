8d272326aa74daf3731e1729f699debf78b9e10c
kms3356 <john563322@gmail.com>
2026-04-08 10:27:12 +0900

    [Silver V] Title: 피보나치 수 4, Time: 64 ms, Memory: 36832 KB -BaekjoonHub

---
+++ 백준/Silver/10826. 피보나치 수 4/피보나치 수 4.py
@@ -0,0 +1,13 @@
+import sys
+sys.setrecursionlimit(10**6)
+def fivo(n):
+    if n==0: return 0
+    if res[n]: return res[n]
+
+    cur = fivo(n-1) + fivo(n-2)
+    res[n] = cur
+    return cur
+
+num = int(input())
+res = [0,1] + [0] * (num-1)
+print(fivo(num))
